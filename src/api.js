ef41b52190fc45dc9e505cf57129f5e2e01c7be9
KayZ <270159822@yoobeestudent.ac.nz>
2022-08-18 23:00:57 +1200

    database.js

--- src/api.js
+++ src/api.js
@@ -1,16 +1,67 @@
 const express = require("express");
+const cors = require('cors')
+
 const serverless = require("serverless-http");
-// const { collection, ObjectId } = require("../config/database")
+const { collection, ObjectId } = require("../config/database")
 
 const app = express();
-
-
 const router = express.Router();
+
+app.use(cors())
+
+// needed for adding a document 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 
 router.get("/", (req, res) => {
     res.send("User List")
+    collection.insertOne(req.body, (err, res) => {
+        if (err) throw err;
+    })
+   res.send("1 document inserted.")
 })
 
+router.post("/", (req, res) => {
+    // res.send("Create New User")
+     collection.insertOne(req.body, (err, res) => {
+         if (err) throw err;
+     })
+    res.send("1 document inserted.")
+ })
+ 
+ router
+     .route("/:id")
+     .get((req, res) => {
+        //res.send(`Get a User with ID: ${req.params.id}`)
+        const query = {
+             _id: ObjectId(req.params.id)
+        }
+        collection.findOne(query, (err, result) => {
+             if (err) throw err;
+             res.send(result)
+        })
+     })
+     .put((req, res) => {
+         //res.send(`Update a User with ID: ${req.params.id}`)
+         const query = { _id: ObjectId(req.params.id) }
+         let newvalues = { $set: req.body };
+         collection.updateOne(query, newvalues, (err, result) => {
+             if (err) throw err;
+         })
+         res.send("1 document updated.")
+     })
+     .delete((req, res) => {
+         //res.send(`Delete a User with ID: ${req.params.id}`)
+         const query = { _id: ObjectId(req.params.id) }
+         collection.deleteOne(query, (err, result) => {
+             if (err) throw err;
+         })
+         res.send("1 document deleted.")
+     })
 
 
 app.use("/.netlify/functions/api", router);
